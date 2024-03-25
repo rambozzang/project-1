963c6c5ddefaf531e1a1409e01b7ff3816b4c19b
judoli3 <141112122+judoli3@users.noreply.github.com>
2024-03-25 10:27:40 +0900

    통합게시판 조회 서비스 수정

--- src/main/java/com/tigerbk/project1/biz/board/ctrl/BoardCtrl.java
+++ src/main/java/com/tigerbk/project1/biz/board/ctrl/BoardCtrl.java
@@ -48,15 +48,38 @@ public class BoardCtrl {
     * @param    : 
     * @return   :
     **/
-    @Operation(summary = "01.통합게시판 조회", description = "\n### 통합게시판 조회 서비스 API")
+    @Operation(summary = "01.통합게시판 병렬형 조회", description = "\n### 통합게시판 조회 서비스 API")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "통합게시판 조회 성공", content = @Content(schema = @Schema(implementation = BoardCvo.BoardInVo.class))),
     })
-    @PostMapping(value = "/board/searchboardlist")
-    public ResponseEntity<?> searchBoardList(@RequestBody BoardCvo.BoardInVo inCvo) throws Exception {
+    @PostMapping(value = "/board/searchParallel")
+    public ResponseEntity<?> searchParallel(@RequestBody BoardCvo.BoardInVo inCvo) throws Exception {
         try {
             BoardSvo.BoardInVo inSvo = cmapper.run(inCvo, BoardSvo.BoardInVo.class);
-            return ResData.SUCCESS(boardSvc.findAllBoardList(inSvo),"통합게시판 조회 정상 조회하였습니다.");
+            return ResData.SUCCESS(boardSvc.findAllPallelList(inSvo),"통합게시판 정상 조회하였습니다.");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            e.printStackTrace();
+            return ResData.FAIL(e.getMessage());
+        }
+    }
+
+    /**
+    *
+    * @name     : BoardCtrl.searchHierichilist
+    * @author   : JuHeon Kim
+    * @param    :
+    * @return   :
+    **/
+    @Operation(summary = "02.통합게시판 계층형 조회", description = "\n### 통합게시판 조회 서비스 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "통합게시판 조회 성공", content = @Content(schema = @Schema(implementation = BoardCvo.BoardInVo.class))),
+    })
+    @PostMapping(value = "/board/searchHierichilist")
+    public ResponseEntity<?> searchHierichilist(@RequestBody BoardCvo.BoardInVo inCvo) throws Exception {
+        try {
+            BoardSvo.BoardInVo inSvo = cmapper.run(inCvo, BoardSvo.BoardInVo.class);
+            return ResData.SUCCESS(boardSvc.findAllHierachiList(inSvo),"통합게시판 정상 조회하였습니다.");
         } catch (Exception e) {
             log.error(e.getMessage());
             e.printStackTrace();
